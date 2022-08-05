607e7cf6dfd5c33913435bfff6886fbf4a41049d
Nguyen Hoang Gia Bao <giabao@evol.vn>
2022-08-05 11:00:43 +0700

    modify test numbers

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest';
 import { transformToNumber } from './numbers';
 
-it("should return a number type if a number string is provided", () => {
+it("should return a number if a number string is provided", () => {
     // Arrange
     const input = '1';
 
@@ -9,19 +9,35 @@ it("should return a number type if a number string is provided", () => {
     const result = transformToNumber(input);
 
     // Assert
-    const expectedResult = 1;
-    expect(result).toBe(expectedResult);
+    // const expectedResult = 1;
+    expect(result).toBe(+input);
 })
 
-it("should return NaN if a string is provided", () => {
+it("should return a number type if a number string is provided", () => {
     // Arrange
-    const input = 'hello';
+
+    const input = '1';
 
     // Act
     const result = transformToNumber(input);
 
     // Assert
+    // const expectedResult = 1;
+    expect(result).toBeTypeOf('number');
+})
+
+it("should return NaN if a string is provided", () => {
+    // Arrange
+    const input = 'hello';
+    const input2 = {};
+
+    // Act
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    // Assert
     expect(result).toBeNaN();
+    expect(result2).toBeNaN();
 })
 
 it('should not to throw if no argument is provided', () => {
