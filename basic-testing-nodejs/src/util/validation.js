ddfc744f35e847247dc2dbbf491a00a6ecc6a609
Nguyen Hoang Gia Bao <giabao@evol.vn>
2022-08-05 14:24:35 +0700

    modify functions to fit test in nodejs project

--- basic-testing-nodejs/src/util/validation.js
+++ basic-testing-nodejs/src/util/validation.js
@@ -5,7 +5,7 @@ function validateStringNotEmpty(value) {
 }
 
 function validateNumber(number) {
-  if (isNaN(number)) {
+  if (typeof number !== 'number') {
     throw new Error('Invalid number input.');
   }
 }
